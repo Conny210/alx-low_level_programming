2fcbe68b83f5017b73a79b2a27bcf6ac4dd76e30
Conny <conny.serite@yahoo.com>
2023-02-16 14:39:19 -0800

    using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-puts("\"Programming is like building a multilingual puzzle\n"); return (0); 
+#include <studio.h>
+/**
+ *main - Entry point
+ *Resturn: Always 0 (Success)
+ */
+ int main(void)
+ {
+	puts("\"Programming is like building a mulilingial puzzle\n"); return (0);
+ }
