0c963a35132cf1305cb2685dec2aaa5559623855
Conny <conny.serite@yahoo.com>
2023-03-10 12:59:52 -0800

    creating a static file

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @x: interger to be tested
+ * Return: absolute value
+ */
+int _abs(int x)
+{
+	if (x < 0)
+		return (-x);
+	return (x);
+}
