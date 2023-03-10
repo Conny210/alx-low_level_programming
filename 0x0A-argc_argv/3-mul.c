3ac1195cc9a3506d59598534ea546a29f6b5915e
Conny <conny.serite@yahoo.com>
2023-03-10 14:35:05 -0800

    program multiplies two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: count of arguments
+ * @argv: pointer of arrays to strings
+ *
+ * Return: 0 (success) else 1 (failure)
+ */
+int main(int argc, char *argv[])
+{
+	int x, sum =1;
+
+	if (argc != 3)
+	{
+		_putchar("Error\n");
+		return (1);
+	}
+	for (x = 1; x < argc; x++)
+		sum *= atoi(argv[i]);
+	_putchar("%d\n", sum);
+	return (0);
+}
