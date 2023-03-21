d1110e23ffdf1cf522ce566cfad72b59c15f4c8e
Conny <conny.serite@yahoo.com>
2023-03-21 03:55:58 -0700

    added descriptions to header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - created new type called struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dogs owner
+ */
+
 typedef struct dog
 {
 	char *name;
