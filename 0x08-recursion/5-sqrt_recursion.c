9cd7369b9d34f13370c4e160ad8f49e6550eac56
Conny <conny.serite@yahoo.com>
2023-08-30 14:38:53 -0700

    updated code to meet betty and gcc requirements

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,51 +1,36 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _sqrt_recursion: Returns the natural square root of a number
+ * _sqrt_recursion - returns natural square root of a number
  *
- * @i: Number
- *
- * Return: The natural square root of 'i' if it exists, -1 otherwise
+ * @d: digit
+ * Return: integer
  */
-
-int _sqrt_recursion(int i);
+int _sqrt_recursion(int d)
 {
-	if (i < 0)
-	{
-		return (-1); /** Error: Negative input */
-	}
-	else if (i == 0 || i == 1)
-	{
-		return (i); /** Base cases: Square of 0 or 1 is itself */
-	}
-	else
-	{
-		return (is_sqrt(i, 2)); /** tests square root from 2 */
-	}
+	int skwer = 2;
+
+	if (d < 0)
+		return (-1);
+	else if (d == 0 || d == 1)
+		return (d);
+	return (is_sqrt(d, skwer));
 }
 
-int is_sqrt(int i, int skwer);
-
 /**
- * is_sqrt: Helper function to check for the natural square root
+ * is_sqrt - function checks for natural square root
  *
- * @i: Number
- * @skwer: Test case
- * Return: The natural square root of 'i' if it exists, -1 otherwise.
+ * @d: digit
+ * @skwer: test number
+ * Return: int
  */
-
+int is_sqrt(int d, int skwer)
 {
-	if (skwer * skwer == i)
-	{
-		return (skwer); /** Found the natural square root */
-	}
-	else if (skwer * skwer < i)
-	{
-		return (is_sqrt(i, skwer + 1)); /** Continue searching */
-	}
-	else
-	{
-		return (-1); /** No natural square root found */
-	}
+	if (skwer * skwer == d)
+		return (skwer);
+	else if (skwer * skwer < d)
+		return (is_sqrt(d, skwer + 1));
+	else if (skwer * skwer > d)
+		return (-1);
+	return (-1);
 }
