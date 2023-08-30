2c612812b9d07216e0f2639f4cc78c90ecb58f35
Conny <conny.serite@yahoo.com>
2023-08-30 12:45:48 -0700

    changed indentations

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,9 +8,9 @@
  */
 int factorial(int x)
 {
-    if (x < 0)
-        return (-1);
-    else if (x == 0)
-        return (1);
-    return (x * factorial(x - 1));
+	if (x < 0)
+		return (-1);
+	else if (x == 0)
+		return (1);
+	return (x * factorial(x - 1));
 }
