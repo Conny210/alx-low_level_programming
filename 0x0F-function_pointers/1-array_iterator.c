4850bf56dee9ed7a918d417831124af01c9aaf68
Conny <conny.serite@yahoo.com>
2023-09-15 13:11:35 -0700

    added program that executes a function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes function
+ *
+ * @array: pointer
+ * @size: size
+ * @action: function
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x;
+
+	for (x = 0; x < size; x++)
+		action(array[x]);
+}
