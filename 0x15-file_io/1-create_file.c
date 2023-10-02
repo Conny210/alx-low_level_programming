d362cd74cb900184a2466a8d192e610d87037f81
Conny <conny.serite@yahoo.com>
2023-10-02 10:14:27 -0700

    added function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "mainn.h"
+
+/**
+ * create_file - function creates file
+ * @filename: pointer to filename
+ * @text_content: content
+ *
+ * Return: 1 on sucess or -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fdesc, error, size = 0;
+
+	if (filename)
+	{
+		fdesc = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+		if (fdesc > 0)
+		{
+			while (text_content && text_content[size] != '\0')
+				size++;
+			error = write(fdesc, text_content, size);
+			close(fdesc);
+			return ((error >= 0) ? 1 : -1);
+		}
+	}
+	return (-1);
+}
